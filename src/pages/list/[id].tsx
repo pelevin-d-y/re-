edb96d6d13b46fa57d7f88a5092128eb7b2cdb4d
Denis <pelevin.d.y@gmail.com>
2021-05-12 23:05:58 +0300

    add getStaticProps to list pag

--- src/pages/list/[id].tsx
+++ src/pages/list/[id].tsx
@@ -1,7 +1,9 @@
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
 import Layout from 'src/layouts/Layout'
 import Content from 'src/components/List/Content'
 import { ListsProvider } from 'src/components/context/ListsContext'
 import 'react-quill/dist/quill.snow.css'
+import testList from 'src/testLists'
 
 const List: React.FC = () => (
   <Layout>
@@ -10,5 +12,29 @@ const List: React.FC = () => (
     </ListsProvider>
   </Layout>
 )
+
+export async function getStaticPaths() {
+  const paths = testList.map((list) => ({
+    params: {
+      id: `${list.id}`,
+    },
+  }))
+  console.log('paths', paths)
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = testList.find(
+    (list) => Number(list.id) === Number(params?.id)
+  )
+  return {
+    props: {
+      postData,
+    },
+  }
+}
 
 export default List
