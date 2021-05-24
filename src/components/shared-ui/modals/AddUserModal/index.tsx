b3fe686aae0d3e070e9371a6e726d2c835cb10aa
Denis <pelevin.d.y@gmail.com>
2021-05-24 11:52:33 +0300

    fix: production build

--- src/components/shared-ui/modals/AddUserModal/index.tsx
+++ src/components/shared-ui/modals/AddUserModal/index.tsx
@@ -34,17 +34,17 @@ const EmailModal: React.FC = () => {
       <div className={s.content}>
         <Formik
           initialValues={{ email: '', password: '' }}
-          validate={values => {
-            const errors = {};
-            if (!values.email) {
-              errors.email = 'Required';
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = 'Invalid email address';
-            }
-            return errors;
-          }}
+          // validate={(values) => {
+          //   const errors = {}
+          //   if (!values.email) {
+          //     errors.email = 'Required'
+          //   } else if (
+          //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          //   ) {
+          //     errors.email = 'Invalid email address'
+          //   }
+          //   return errors
+          // }}
           onSubmit={(values, { setSubmitting }) => {
             setTimeout(() => {
               alert(JSON.stringify(values, null, 2));
