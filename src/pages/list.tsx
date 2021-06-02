2d15185b7d468ee9a50632b46eec3289c1ecd6f6
Denis <pelevin.d.y@gmail.com>
2021-06-02 20:42:45 +0300

    feat: update list page for new lists

---
+++ src/pages/list.tsx
@@ -0,0 +1,28 @@
+import Layout from 'src/layouts/Layout'
+import Content from 'src/components/List/Content'
+import 'react-quill/dist/quill.snow.css'
+import { PopupProvider } from 'src/components/context/PopupContext'
+import { TemplatesProvider } from 'src/components/context/TemplatesContext'
+import EmailModal from 'src/components/shared-ui/modals/ContactModal'
+import { css } from 'astroturf'
+import MultiEmailsModal from 'src/components/shared-ui/modals/MultiEmailsModal'
+
+const List: React.FC = () => (
+  <TemplatesProvider>
+    <PopupProvider>
+      <Layout className={s.layout}>
+        <Content />
+        <EmailModal />
+        <MultiEmailsModal />
+      </Layout>
+    </PopupProvider>
+  </TemplatesProvider>
+)
+
+const s = css`
+  .layout {
+    background: var(--white);
+  }
+`
+
+export default List
