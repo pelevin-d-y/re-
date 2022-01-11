511743fe5c178b25266f46dad2e539f09dd15044
Denis Pelevin <pelevin.d.y@gmail.com>
2022-01-11 18:01:28 +0300

    remove unused icons and images

--- src/helpers/utils/add-addition-fields.ts
+++ src/helpers/utils/add-addition-fields.ts
@@ -2,23 +2,12 @@ import findTemplate from 'src/helpers/utils/find-template'
 import { calculateColorByStraight } from 'src/helpers/utils/calculate-strength'
 import testTemplates from 'src/testTemplates.json'
 
-const avatars = [
-  'gino.jpeg',
-  'maker.jpeg',
-  'james.png',
-  'mary.jpeg',
-  'phil.jpeg',
-  'steve.jpeg',
-]
-
 const addAdditionFields = (users: RecommendationUser[]): UserData[] => {
   const extendedUsers: UserData[] = users.map((user) => ({
     ...user,
     avatar: user.contact_id
       ? `https://d1vi0xe0gbvwgr.cloudfront.net/contact_images/${user.contact_id}.jpg`
       : '',
-    title: 'Investors', // field used in the list table
-    pinned: false,
     templateData: findTemplate(testTemplates, user.template),
     relationshipStrength: calculateColorByStraight(user.connection_E),
   }))
