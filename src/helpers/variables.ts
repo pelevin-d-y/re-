dc8b7efcae4c5c740ebadcfed9888eb91022bec0
Denis Pelevin <pelevin.d.y@gmail.com>
2022-01-14 17:42:49 +0300

    feat: contact detail page, get data from mutable data

--- src/helpers/variables.ts
+++ src/helpers/variables.ts
@@ -2,5 +2,8 @@ const logInLink =
   'https://strata.auth.us-east-1.amazoncognito.com/login?client_id=4i8sdfh90s4gutrec91couh6e5&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://app.strata.cc'
 
 const LS_ID_TOKEN = 'strata_id_token'
+const getAvatarUrl = (id: string): string =>
+  `https://d1vi0xe0gbvwgr.cloudfront.net/contact_images/${id}.jpg`
+const primaryEmailType = 'primaryEmail'
 
-export { logInLink, LS_ID_TOKEN }
+export { logInLink, LS_ID_TOKEN, getAvatarUrl, primaryEmailType }
