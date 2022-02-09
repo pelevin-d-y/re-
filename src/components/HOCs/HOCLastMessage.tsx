d5c6582ae4a54da37c926f735f48307ef8218cc8
Denis Pelevin <pelevin.d.y@gmail.com>
2022-02-09 18:33:23 +0300

    add delay loading for last message, fix wrong last message in table

--- src/components/HOCs/HOCLastMessage.tsx
+++ src/components/HOCs/HOCLastMessage.tsx
@@ -1,11 +1,5 @@
-import {
-  useEffect,
-  useState,
-  ComponentType,
-  useRef,
-  forwardRef,
-  ReactElement,
-} from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState, ReactElement, useRef } from 'react'
 import { useInView } from 'react-intersection-observer'
 import { get } from 'src/api'
 import formatLastMessage from 'src/helpers/utils/format-last-message'
@@ -22,33 +16,44 @@ type Props = {
 }
 
 const HOCLastMessage: React.FC<Props> = ({ children, id }) => {
-  const { ref, inView } = useInView()
+  const { ref, inView } = useInView({
+    delay: 1400,
+  })
 
   const [isLoading, setIsLoading] = useState(false)
+  const prevIdRef = useRef(id)
   const [lastMessageData, setLastMessageData] = useState<any>(null)
+
+  const fetchLastEmail = async () => {
+    setIsLoading(true)
+    await get
+      .getLastEmails([id])
+      .then((res) => {
+        const lastMessageResponse = getLastMessage(res[id])
+        const contactLastMessage = formatLastMessage(lastMessageResponse)
+        setLastMessageData(contactLastMessage)
+        setIsLoading(false)
+      })
+      .catch((err) => {
+        if (err.response.status === 502) {
+          fetchLastEmail()
+        }
+        setIsLoading(false)
+      })
+  }
 
   useEffect(() => {
     if (inView && !lastMessageData) {
-      const fetchLastEmail = async () => {
-        setIsLoading(true)
-        await get
-          .getLastEmails([id])
-          .then((res) => {
-            const lastMessageResponse = getLastMessage(res[id])
-            const contactLastMessage = formatLastMessage(lastMessageResponse)
-            setLastMessageData(contactLastMessage)
-            setIsLoading(false)
-          })
-          .catch((err) => {
-            if (err.response.status === 502) {
-              fetchLastEmail()
-            }
-            setIsLoading(false)
-          })
-      }
       fetchLastEmail()
     }
   }, [id, inView, lastMessageData])
+
+  useEffect(() => {
+    if (prevIdRef.current !== id) {
+      fetchLastEmail()
+      prevIdRef.current = id
+    }
+  }, [id])
 
   return children(lastMessageData, isLoading, ref)
 }
