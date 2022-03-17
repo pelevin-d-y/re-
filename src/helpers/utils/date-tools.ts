98e2b23ac0aee75e3488d5e63a3fa3c8a44db2fc
Italo Lelis <italo.carvalho@natahouse.com>
2022-03-17 13:43:22 -0300

    fix: show last week on calendar

--- src/helpers/utils/date-tools.ts
+++ src/helpers/utils/date-tools.ts
@@ -7,3 +7,19 @@ export const isDateBeforeLastMonday = (date: Date): boolean => {
 
   return isAfter(date, lastMonday)
 }
+
+export const getLastWeekMonday = (): Date => {
+  const lastMonday = new Date()
+  lastMonday.setDate(lastMonday.getDate() - lastMonday.getDay() + 1 - 7)
+  lastMonday.setHours(0, 0, 0, 0)
+
+  return lastMonday
+}
+
+export const getLastWeekSunday = (): Date => {
+  const lastSunday = new Date()
+  lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay() + 7 - 7)
+  lastSunday.setHours(0, 0, 0, 0)
+
+  return lastSunday
+}
